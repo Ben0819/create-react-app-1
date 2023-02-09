876cfc058bf5a4d67fab8f8a359460c927be794b
Ben0819 <116129299+Ben0819@users.noreply.github.com>
2023-02-09 13:10:25 -0500

    ben

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
 
 <div>
     <div class="outline">
-      <div class="lecard">
+      <div class="card">
         <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
+        <img src="https://sportshub.cbsistatic.com/i/r/2022/06/10/139ab78f-d458-4a66-9179-0312585bfbfa/thumbnail/1200x675/f403faf31c079bf1f1206f8e8b96660e/tastumcbs.jpg" />
+        <p class="details">NBA FINALS <br></br> 2022 Warriors <i>VS Celtics</i></p>
       </div>
     </div>
   <div class="buttons">
